6b16d2cdc9c5eb7a961e5edc2550b5897ac6db1a
Carlos Nakid <carlos.nakidg@gmail.com>
2026-08-10 11:16:26 -0600

    Modified landing

--- backend/app.py
+++ backend/app.py
@@ -177,5 +177,5 @@ def serve_resume():
         Path("CarlosNakidResume.pdf"),
         media_type="application/pdf",
         filename="CarlosNakidResume.pdf",
-        as_attachment=False
+        content_disposition_type="inline"
     )
